498459b538a1cd784166d79863790cf405820418
yousef0090 <54067011+yousef0090@users.noreply.github.com>
2019-08-16 01:23:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== "552496297429565440") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
